8f7c12a1da22d9ebf1f08ad37e44d64a3fe17c02
Sebbe213 <sebastianraoof@hotmail.se>
2024-09-23 19:40:40 +0200

    Made a fxml file to start

--- src/main/java/org/team6/Main.java
+++ src/main/java/org/team6/Main.java
@@ -1,7 +1,27 @@
 package org.team6;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Correct resource path for FXML file based on your structure
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/org/team6/MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        primaryStage.setTitle("Homepage");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
+
         System.out.println("Hello world!");
     }
 }
