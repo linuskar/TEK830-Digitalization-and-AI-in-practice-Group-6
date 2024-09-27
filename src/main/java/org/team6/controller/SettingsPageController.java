a16c8284df78bfbe4f5c547c39e08df7e821e8d2
Caramajau <caramia0505@gmail.com>
2024-09-27 17:16:43 +0200

    Added another toggle option

--- src/main/java/org/team6/controller/SettingsPageController.java
+++ src/main/java/org/team6/controller/SettingsPageController.java
@@ -6,14 +6,28 @@ import javafx.scene.control.ToggleButton;
 public class SettingsPageController {
     @FXML
     private ToggleButton sendNotificationsToggleButton;
+    @FXML
+    private ToggleButton sendLowElectricPriceToggleButton;
 
+    // Meant for toggling notifications in general.
     @FXML
     private void handleSendNotificationsOnAction() {
         boolean isSelected = sendNotificationsToggleButton.isSelected();
         if (isSelected) {
-            // NotificationModel.EnableNotifications()
+            // NotificationModel.enableNotifications()
         } else {
-            // NotificationModel.DisableNotifications()
+            // NotificationModel.disableNotifications()
+        }
+    }
+
+    // Example of specifically toggling a notification.
+    @FXML
+    private void handleLowElectricPriceOnAction() {
+        boolean isSelected = sendLowElectricPriceToggleButton.isSelected();
+        if (isSelected) {
+            // NotificationModel.enableNotification(NotificationType.LOW_ELECTRIC)
+        } else {
+            // NotificationModel.disableNotification(NotificationType.LOW_ELECTRIC)
         }
     }
 
