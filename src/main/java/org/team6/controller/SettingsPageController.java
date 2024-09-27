4769b39ed84393dd8e73217fe55af8ab689537b6
Caramajau <caramia0505@gmail.com>
2024-09-27 12:59:54 +0200

    Created super simple settings page

---
+++ src/main/java/org/team6/controller/SettingsPageController.java
@@ -0,0 +1,20 @@
+package org.team6.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+
+public class SettingsPageController {
+    @FXML
+    private ToggleButton sendNotificationsToggleButton;
+
+    @FXML
+    private void handleSendNotificationsOnAction() {
+        boolean isSelected = sendNotificationsToggleButton.isSelected();
+        if (isSelected) {
+            // NotificationModel.EnableNotifications()
+        } else {
+            // NotificationModel.DisabledNotifications()
+        }
+    }
+
+}
