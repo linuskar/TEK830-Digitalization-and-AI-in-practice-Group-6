caca0c078b8f3ac807f9e42f50fe24738723d160
Caramajau <caramia0505@gmail.com>
2024-09-29 13:08:59 +0200

    Moved enums to seperate files

---
+++ src/main/java/org/team6/Notifications.java
@@ -0,0 +1,8 @@
+package org.team6;
+
+public enum Notifications {
+    ONE,
+    TWO,
+    THREE,
+    FOUR
+}
---
+++ src/main/java/org/team6/Sound.java
@@ -0,0 +1,8 @@
+package org.team6;
+
+public enum Sound {
+    VIBRATIONS,
+    LOW,
+    MEDIUM,
+    HIGH
+}
