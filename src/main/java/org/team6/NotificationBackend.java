d3c0a1dfc6c0b2c7538f080ccf0783398a56aa49
Caramajau <caramia0505@gmail.com>
2024-09-29 13:45:34 +0200

    One variable names are the worst
    Sometimes it is acceptable when it is conventional though

--- src/main/java/org/team6/NotificationBackend.java
+++ src/main/java/org/team6/NotificationBackend.java
@@ -4,17 +4,17 @@ public class NotificationBackend {
     // Static classes shouldn't expose constructor.
     private NotificationBackend() {}
 
-    public static void turnOnTurnOffAllNotifications(User u) {
-        u.toggleNotifications();
+    public static void turnOnTurnOffAllNotifications(User user) {
+        user.toggleNotifications();
     }
 
-    public static void turnOnTurnOffASpecificNotification(User u, Notification type) {
-        if (u.areNotificationsOn()) {
-            u.toggleSpecificNotification(type);
+    public static void turnOnTurnOffASpecificNotification(User user, Notification type) {
+        if (user.areNotificationsOn()) {
+            user.toggleSpecificNotification(type);
         }
     }
 
-    public static void changeSoundLevel(User u, SoundLevel type) {
-        u.setSoundLevel(type);
+    public static void changeSoundLevel(User user, SoundLevel type) {
+        user.setSoundLevel(type);
     }
 }
