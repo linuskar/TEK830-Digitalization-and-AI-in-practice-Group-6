26e422aa13b617f8a08f22a24226528af1184d5c
Linus Karlsson <linuskungalv@gmail.com>
2024-10-01 20:15:19 +0200

    fix

--- src/main/java/org/team6/view/App.java
+++ src/main/java/org/team6/view/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.team6.Main;
+import org.team6.controller.MainPageController;
 
 import java.io.IOException;
 
@@ -13,7 +14,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/org/team6/view/MainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
+        MainPageController mainViewController = fxmlLoader.getController();
+        mainViewController.setupKeyHandling(scene);
+        
         primaryStage.setTitle("Homepage");
         primaryStage.setScene(scene);
         primaryStage.show();
