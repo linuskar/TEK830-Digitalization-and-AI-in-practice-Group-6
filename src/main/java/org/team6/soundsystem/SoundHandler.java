6ceb2614d07b42e889fd0d18afff5f61245d2e67
Caramajau <caramia0505@gmail.com>
2024-10-05 17:38:08 +0200

    Start to a sound system but again

--- src/main/java/org/team6/soundsystem/SoundHandler.java
+++ src/main/java/org/team6/soundsystem/SoundHandler.java
@@ -1,4 +1,30 @@
 package org.team6.soundsystem;
 
-public class SoundHandler {
+import org.team6.Main;
+import org.team6.model.Notification;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+class SoundHandler {
+    private final Map<Notification, Media> notificationSoundMap = new EnumMap<>(Notification.class);
+
+    public SoundHandler() {
+        for (Notification notification : Notification.values()) {
+            String soundToPlay = Main.class.getResource("/org/team6/sounds/test.mp3").toExternalForm();
+            Media sound = new Media(soundToPlay);
+            notificationSoundMap.put(notification, sound);
+        }
+    }
+
+    public void playSound(Notification notification) {
+        Media sound = notificationSoundMap.get(notification);
+        if (sound != null) {
+            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.play();
+        }
+    }
 }
