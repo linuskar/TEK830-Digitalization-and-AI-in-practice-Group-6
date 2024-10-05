a9ec4ada4d842249967420d97ca46cfbb71a0052
Caramajau <caramia0505@gmail.com>
2024-10-05 17:59:34 +0200

    Extracted method in SoundHandler

--- src/main/java/org/team6/soundsystem/SoundHandler.java
+++ src/main/java/org/team6/soundsystem/SoundHandler.java
@@ -6,18 +6,26 @@ import org.team6.model.Notification;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.net.URL;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 
 class SoundHandler {
     private final Map<Notification, Media> notificationSoundMap = new EnumMap<>(Notification.class);
+    private static final String SOUND_PATH = "/org/team6/sounds/test.mp3";
 
     public SoundHandler() {
         for (Notification notification : Notification.values()) {
-            String soundToPlay = Main.class.getResource("/org/team6/sounds/test.mp3").toExternalForm();
-            Media sound = new Media(soundToPlay);
+            Media sound = getSoundFromPath(SOUND_PATH);
             notificationSoundMap.put(notification, sound);
         }
+    }
+
+    private static Media getSoundFromPath(String soundPath) {
+        URL soundUrl = Objects.requireNonNull(Main.class.getResource(soundPath));
+        String soundToPlay = soundUrl.toExternalForm();
+        return new Media(soundToPlay);
     }
 
     public void playSound(Notification notification) {
