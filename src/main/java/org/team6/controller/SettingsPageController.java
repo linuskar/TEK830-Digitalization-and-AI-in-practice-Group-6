59e0359cab65f2e0bd2306fb0e9cff271a0aae04
Caramajau <caramia0505@gmail.com>
2024-10-08 16:59:21 +0200

    Changed notificationButtons to a Map

--- src/main/java/org/team6/controller/SettingsPageController.java
+++ src/main/java/org/team6/controller/SettingsPageController.java
@@ -1,5 +1,6 @@
 package org.team6.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,8 +12,8 @@ import org.team6.model.NotificationBackend;
 import org.team6.view.PageStarter;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class SettingsPageController implements Initializable {
@@ -35,16 +36,17 @@ public class SettingsPageController implements Initializable {
     // Nicer to show the user a scale from 0 to 100 rather than 0 to 1.
     private static final int VOLUME_SCALE_FACTOR = 100;
 
-    // List of specific notification buttons. For instance used for
+    // Map of specific notification buttons. For instance used for
     // enabling and disabling all buttons if notifications are switched on or off.
-    private final List<ToggleButton> notificationButtons = new ArrayList<>();
+    private final Map<ToggleButton, Notification> notificationButtons = new HashMap<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        notificationButtons.add(sendLowElectricPriceToggleButton);
-        notificationButtons.add(sendHighElectricPriceToggleButton);
-        notificationButtons.add(sendSunnyWeatherToggleButton);
-        notificationButtons.add(sendColdWeatherToggleButton);
+        notificationButtons.put(sendLowElectricPriceToggleButton, Notification.LOW_ELECTRICITY_PRICE);
+        notificationButtons.put(sendHighElectricPriceToggleButton, Notification.HIGH_ELECTRICITY_PRICE);
+        notificationButtons.put(sendSunnyWeatherToggleButton, Notification.SUNNY_WEATHER);
+        notificationButtons.put(sendColdWeatherToggleButton, Notification.COLD_WEATHER);
+
         initVolumeSlider();
         initToggleButtons();
     }
@@ -76,30 +78,21 @@ public class SettingsPageController implements Initializable {
     private void toggleNotificationButtonsAvailability() {
         boolean isSelected = sendNotificationsToggleButton.isSelected();
 
-        for (ToggleButton notificationButton : notificationButtons) {
+        for (ToggleButton notificationButton : notificationButtons.keySet()) {
             notificationButton.setDisable(!isSelected);
         }
     }
 
-    // Example of specifically toggling a notification.
     @FXML
-    private void handleLowElectricPriceOnAction() {
-        NotificationBackend.toggleASpecificNotification(Notification.LOW_ELECTRICITY_PRICE);
-    }
+    private void handleSpecificNotificationOnAction(ActionEvent event) {
+        ToggleButton sourceButton = (ToggleButton) event.getSource();
 
-    @FXML
-    private void handleHighElectricPriceOnAction() {
-        NotificationBackend.toggleASpecificNotification(Notification.HIGH_ELECTRICITY_PRICE);
-    }
-
-    @FXML
-    private void handleSunnyWeatherOnAction() {
-        NotificationBackend.toggleASpecificNotification(Notification.SUNNY_WEATHER);
-    }
-
-    @FXML
-    private void handleColdWeatherPriceOnAction() {
-        NotificationBackend.toggleASpecificNotification(Notification.COLD_WEATHER);
+        if (notificationButtons.containsKey(sourceButton)) {
+            Notification notificationToBeToggled = notificationButtons.get(sourceButton);
+            NotificationBackend.toggleASpecificNotification(notificationToBeToggled);
+        } else {
+            System.err.println("Event button" + sourceButton + "does not exist in Map!");
+        }
     }
 
     @FXML
