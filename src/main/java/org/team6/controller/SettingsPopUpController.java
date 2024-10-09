07e01a431dec0400f1427c300c07f28acee91005
Caramajau <caramia0505@gmail.com>
2024-10-09 13:03:15 +0200

    Simplified SettingsPopUpController

--- src/main/java/org/team6/controller/SettingsPopUpController.java
+++ src/main/java/org/team6/controller/SettingsPopUpController.java
@@ -1,22 +1,11 @@
 package org.team6.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import org.team6.model.NotificationBackend;
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-public class SettingsPopUpController implements Initializable {
+public class SettingsPopUpController {
     @FXML
     private AnchorPane popUpPane;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        popUpPane.setVisible(true);
-    }
 
     @FXML
     private void hidePopUp() {
