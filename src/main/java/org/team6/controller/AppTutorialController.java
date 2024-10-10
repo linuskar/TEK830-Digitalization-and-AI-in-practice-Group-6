c7c1d0ccfb0869fa0a6cd55b32e1e955a0ce255e
Caramajau <caramia0505@gmail.com>
2024-10-10 12:04:24 +0200

    Connected UI elements with IDs

--- src/main/java/org/team6/controller/AppTutorialController.java
+++ src/main/java/org/team6/controller/AppTutorialController.java
@@ -1,5 +1,44 @@
 package org.team6.controller;
 
-public class AppTutorialController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AppTutorialController implements Initializable {
+    @FXML
+    private Label counterLabel;
+
+    @FXML
+    private ImageView tutorialImage;
+
+    @FXML
+    private Label infoText;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private Button forwardButton;
+    @FXML
+    private Button backButton;
+
+    private int currentPageNumber = 0;
+    private final List<String> pageInfoList = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setCounterLabelText(currentPageNumber);
+    }
+
+    private void setCounterLabelText(int currentPageNumber) {
+        int totalPageNumber = pageInfoList.size();
+        counterLabel.setText(currentPageNumber + "/" + totalPageNumber);
+    }
 }
