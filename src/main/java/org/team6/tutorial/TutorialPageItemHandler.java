b4cb55049e1b9cd22e80958e5fc598fc335b7371
Caramajau <caramia0505@gmail.com>
2024-10-10 15:54:10 +0200

    Added info files

--- src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
+++ src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
@@ -8,14 +8,17 @@ import java.util.List;
 
 public class TutorialPageItemHandler {
     private final List<TutorialPageItem> tutorialPageItems = new ArrayList<>();
-    private static final String TEXT_FILE_PATH = "";
-    private static final String IMAGE_FILE_PATH = "";
+    private static final String TEXT_FILE_PATH = "org/team6/texts/";
+    private static final String IMAGE_FILE_PATH = "org/team6/images/tutorial";
     private static final int ITEM_COUNT = 5;
 
     public TutorialPageItemHandler() {
         for (int i = 0; i < ITEM_COUNT; i++) {
-            String itemText = TextFileReader.readFileAsString(TEXT_FILE_PATH);
-            Image itemImage = new Image(IMAGE_FILE_PATH);
+            String textFileName = "info" + i + ".md";
+            String itemText = TextFileReader.readFileAsString(TEXT_FILE_PATH + textFileName);
+
+            String imageFileName = "info" + i + ".png";
+            Image itemImage = new Image(IMAGE_FILE_PATH + imageFileName);
 
             TutorialPageItem itemToAdd = new TutorialPageItem(itemText, itemImage);
             tutorialPageItems.add(itemToAdd);
