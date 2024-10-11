d253db9c0d4ea039b43049b5cf8a05683364851a
Caramajau <caramia0505@gmail.com>
2024-10-11 16:54:54 +0200

    More improvements to TutorialPageItemHandler

--- src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
+++ src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
@@ -7,35 +7,45 @@ import org.team6.view.ImageUtils;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TutorialPageItemHandler {
+    private static final Logger logger = Logger.getLogger(TutorialPageItemHandler.class.getName());
+
     private final List<TutorialPageItem> tutorialPageItems = new ArrayList<>();
     private static final String TEXT_FILE_PATH = "/org/team6/texts/";
     private static final String IMAGE_FILE_PATH = "/org/team6/images/tutorial/";
 
     public TutorialPageItemHandler() {
-        File[] textFiles = getTextFiles(TEXT_FILE_PATH, "info\\d+\\.md");
-        File[] imageFiles = getTextFiles(IMAGE_FILE_PATH, "info\\d+\\.png");
+        File[] textFiles = getFiles(TEXT_FILE_PATH, "info\\d+\\.md");
+        File[] imageFiles = getFiles(IMAGE_FILE_PATH, "info\\d+\\.png");
+        if (textFiles == null || imageFiles == null) {
+            return;
+        }
 
-        if (textFiles != null && imageFiles != null) {
-            for (File textFile : textFiles) {
-                String textFileName = textFile.getName();
-                String itemText = TextFileReader.readFileAsString(TEXT_FILE_PATH + textFileName);
+        for (int i = 0; i < textFiles.length; i++) {
+            String textFileName = textFiles[i].getName();
+            String itemText = TextFileReader.readFileAsString(TEXT_FILE_PATH + textFileName);
 
-                String imageFileName = textFileName.replace(".md", ".png");
-                String imageFileString = ImageUtils.getFileResourceString(IMAGE_FILE_PATH + imageFileName);
+            String imageFileName = imageFiles[i].getName();
+            String imageFileString = ImageUtils.getFileResourceString(IMAGE_FILE_PATH + imageFileName);
 
-                Image itemImage = new Image(imageFileString);
+            Image itemImage = new Image(imageFileString);
 
-                TutorialPageItem itemToAdd = new TutorialPageItem(itemText, itemImage);
-                tutorialPageItems.add(itemToAdd);
-            }
+            TutorialPageItem itemToAdd = new TutorialPageItem(itemText, itemImage);
+            tutorialPageItems.add(itemToAdd);
         }
     }
 
-    private File[] getTextFiles(String filePath, String fileNameRegex) {
-        File fileDir = new File(getClass().getResource(filePath).getFile());
-        return fileDir.listFiles((dir, name) -> name.matches(fileNameRegex));
+    private File[] getFiles(String filePath, String fileNameRegex) {
+        File fileDir = new File(Objects.requireNonNull(getClass().getResource(filePath)).getFile());
+        File[] files = fileDir.listFiles((dir, name) -> name.matches(fileNameRegex));
+        if (files == null) {
+            logger.log(Level.SEVERE, () -> "No files found in: " + filePath);
+        }
+        return files;
     }
 
     public TutorialPageItem getTutorialPageItemAt(int index) {
