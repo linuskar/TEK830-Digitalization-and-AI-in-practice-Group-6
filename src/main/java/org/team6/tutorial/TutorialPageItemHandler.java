0c70a4c3a6676565b06f6499038dac3b119d8ab6
Caramajau <caramia0505@gmail.com>
2024-10-11 17:55:20 +0200

    made TutorialPageItemHandler less extendable
    The other version doesn't work as jar

--- src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
+++ src/main/java/org/team6/tutorial/TutorialPageItemHandler.java
@@ -4,35 +4,21 @@ import javafx.scene.image.Image;
 import org.team6.utility.TextFileReader;
 import org.team6.view.ImageUtils;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TutorialPageItemHandler {
-    private static final Logger logger = Logger.getLogger(TutorialPageItemHandler.class.getName());
-
     private final List<TutorialPageItem> tutorialPageItems = new ArrayList<>();
     private static final String TEXT_FILE_PATH = "/org/team6/texts/";
     private static final String IMAGE_FILE_PATH = "/org/team6/images/tutorial/";
+    private static final int ITEM_COUNT = 5;
 
     public TutorialPageItemHandler() {
-        File[] textFiles = getFiles(TEXT_FILE_PATH, "info\\d+\\.md");
-        File[] imageFiles = getFiles(IMAGE_FILE_PATH, "info\\d+\\.png");
-        if (textFiles == null || imageFiles == null) {
-            return;
-        } else if (textFiles.length != imageFiles.length) {
-            logger.log(Level.SEVERE, () -> "Number of text files and image files do not match." + "\nText files: " + textFiles.length + "\nImage files: " + imageFiles.length);
-            return;
-        }
-
-        for (int i = 0; i < textFiles.length; i++) {
-            String textFileName = textFiles[i].getName();
+        for (int i = 0; i < ITEM_COUNT; i++) {
+            String textFileName = "info" + i + ".md";
             String itemText = TextFileReader.readFileAsString(TEXT_FILE_PATH + textFileName);
 
-            String imageFileName = imageFiles[i].getName();
+            String imageFileName = "info" + i + ".png";
             String imageFileString = ImageUtils.getFileResourceString(IMAGE_FILE_PATH + imageFileName);
 
             Image itemImage = new Image(imageFileString);
@@ -40,15 +26,6 @@ public class TutorialPageItemHandler {
             TutorialPageItem itemToAdd = new TutorialPageItem(itemText, itemImage);
             tutorialPageItems.add(itemToAdd);
         }
-    }
-
-    private File[] getFiles(String filePath, String fileNameRegex) {
-        File fileDir = new File(Objects.requireNonNull(getClass().getResource(filePath)).getFile());
-        File[] files = fileDir.listFiles((dir, name) -> name.matches(fileNameRegex));
-        if (files == null) {
-            logger.log(Level.SEVERE, () -> "No files found in: " + filePath);
-        }
-        return files;
     }
 
     public TutorialPageItem getTutorialPageItemAt(int index) {
