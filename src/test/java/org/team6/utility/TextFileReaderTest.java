a541e1246c59251e2b8674eeff1b3d53122cf71e
Figer-op <90894397+Figer-op@users.noreply.github.com>
2024-10-16 14:42:43 +0200

    removed an enetr

--- src/test/java/org/team6/utility/TextFileReaderTest.java
+++ src/test/java/org/team6/utility/TextFileReaderTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
