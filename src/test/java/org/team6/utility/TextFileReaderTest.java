6c31ba69241b76737c1fe1255e3087c4833f7d76
Figer-op <90894397+Figer-op@users.noreply.github.com>
2024-10-16 14:40:53 +0200

    removed some impoarts

--- src/test/java/org/team6/utility/TextFileReaderTest.java
+++ src/test/java/org/team6/utility/TextFileReaderTest.java
@@ -2,16 +2,14 @@ package org.team6.utility;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
-public class TextFileReaderTest {
+class TextFileReaderTest {
     
     @ParameterizedTest
     @EnumSource(TextPath.class)
