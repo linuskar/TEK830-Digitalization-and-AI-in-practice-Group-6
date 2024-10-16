e4fbc87b739d3f10d6f7b5b24f3edd436a3e7723
Caramajau <caramia0505@gmail.com>
2024-10-16 13:27:02 +0200

    Created tests for image path enum

---
+++ src/test/java/org/team6/view/ImagePathTest.java
@@ -0,0 +1,16 @@
+package org.team6.view;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+@ExtendWith(ApplicationExtension.class)
+class ImagePathTest {
+    @ParameterizedTest
+    @EnumSource(ImagePath.class)
+    void testGetImage_ForALLImagePath_ReturnsImage(ImagePath imagePath) {
+        assertNotNull(imagePath.getImage());
+    }
+}
