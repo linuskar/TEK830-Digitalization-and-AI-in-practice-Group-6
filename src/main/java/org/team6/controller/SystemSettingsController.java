e6bb64dfc276078a41e1ca8040c1fd64071d3955
Sebbe213 <sebastianraoof@hotmail.se>
2024-10-21 14:48:25 +0200

    Added some functionality

--- src/main/java/org/team6/controller/SystemSettingsController.java
+++ src/main/java/org/team6/controller/SystemSettingsController.java
@@ -1,6 +1,25 @@
 package org.team6.controller;
 
+import javafx.fxml.FXML;
+import org.team6.view.PageStarter;
+
 public class SystemSettingsController {
+
+    @FXML
+    private void handleHomeButtonOnAction(){
+        PageStarter.switchToHomePage();
+    }
+
+    @FXML
+    private void handleEnergyPageButtonOnAction(){
+        PageStarter.switchToEnergyPage();
+    }
+
+    @FXML
+    private void handleSystemSettingsButtonOnAction(){
+        PageStarter.switchToSystemSettings();
+    }
+
 
 
 }
