5cd6b5235770f9d11bf55d3c3337454f185acf8b
Caramajau <caramia0505@gmail.com>
2024-10-30 12:39:38 +0100

    Removed FXML tag in notification history

--- src/main/java/org/team6/model/NotificationHistory.java
+++ src/main/java/org/team6/model/NotificationHistory.java
@@ -3,14 +3,11 @@ package org.team6.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.fxml.FXML;
-
 public class NotificationHistory {
     private static final int MAX_NOTIFICATION_AMOUNT = 5;
     private final List<Notification> sentNotifications = new ArrayList<>(MAX_NOTIFICATION_AMOUNT);
 
     // Adds notification to a list.
-    @FXML
     public void addNotification(Notification notification) {
         if (sentNotifications.size() == MAX_NOTIFICATION_AMOUNT) {
             sentNotifications.remove(0);  // Remove the oldest notification
